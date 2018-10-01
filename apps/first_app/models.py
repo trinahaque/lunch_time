b4d097aa6cdf385dc0976c7a43161ea4b34b6a3e
trina <trina_haque28@outlook.com>
2018-09-30 22:04:16 -0700

    Add comments to clarify methods and codes

--- apps/first_app/models.py
+++ apps/first_app/models.py
@@ -7,12 +7,14 @@ from datetime import datetime, date
 Email_Regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class UserManager(models.Manager):
+    
     # registrations takes the input from the views and creates a new user
     def registration(self, request):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         errors = []
-
+        
+        # if user leaves a field empty when filling out the form
         if len(first_name) < 1 or len(last_name) < 1:
             errors.append("A field can not be empty")
         else:
@@ -29,46 +31,33 @@ class UserManager(models.Manager):
     def getCoffee(self, pid):
         # retrieve the user in session
         user = User.objects.get(id=pid)
-        # print "userId", user.id
-        
-        # receive a list of all users excluding the user itself 
+       
+        # receive a list of all users excluding the current user
         userList = User.objects.exclude(id=pid)
-        # print "userList", userList
-
+        
         # retrieve all the friends met for coffee before
         oldCoffeeFriends = user.coffee_friends.all()
-        # print "user before", user.id
-        # print "coffee friend", user.coffee_friends
-        # print "all coffee", oldCoffeeFriends
-        # for each in oldCoffeeFriends:
-        #     print "old", each.first_name
-
-        # remove old coffee friends from the friends list to 
-        # find a list of new friends
-        newFriendsList = []
-
-        for nonFriend in userList:
-            valid = True
-            for friend in oldCoffeeFriends:
-                if (nonFriend.id == friend.id):
-                    valid = False
-                    break
-            if valid:
-                newFriendsList.append(nonFriend)
-        # print "new friends", newFriendsList
         
+        # remove old coffee friends from the userlist to 
+        # find a list of new friends
+        friends_to_exclude = [friend.id for friend in oldCoffeeFriends]
+        
+        # newFriendsList provides list of all the friends not met for coffee before
+        newFriendsList = User.objects.exclude(id__in=friends_to_exclude).exclude(id=pid)
+
         error = []
+        
+        # newFriendsList will be empty if the user has met with everyone for coffee
         if len(newFriendsList) < 1:
             error.append("You have met with everyone for coffee. Please get lunch this time")
             return (False, error)
         else: 
             newCoffeeFriend = random.choice(newFriendsList)
-            # print "coffeeFriendId", newCoffeeFriend.first_name
-          
             user.coffee_friends.add(newCoffeeFriend)
-           
             return (True, newCoffeeFriend.id)
 
+    # this function takes user id (pid), the range of number of friends acceptable for a lunch group
+    # it generate a lunch group for the user in session
     def getLunch(self, pid, min_friend_number, max_friend_number):
         min_friend_number = min_friend_number
         max_friend_number = max_friend_number
@@ -81,7 +70,6 @@ class UserManager(models.Manager):
         friends_to_exclude = [friend.id for friend in oldLunchFriends]
         # newFriendsList provides list of all the friends not met for lunch before
         newFriendsList = User.objects.exclude(id__in=friends_to_exclude).exclude(id=pid)
-        # print "newFriendsList", newFriendsList
 
         # generates a number between 2 and 4 inclusive
         count_of_friends = random.randint(min_friend_number, max_friend_number) 
@@ -90,7 +78,6 @@ class UserManager(models.Manager):
         # ask user to wait
         if len(userList) < 1:
             errors.append("You are the only one in the system. Please wait for others to join.")
-            print "1", errors[0]
             return (False, errors)
         
         # if application only has 3 or 2 users, lunch group will have all the users in the system
@@ -98,45 +85,30 @@ class UserManager(models.Manager):
             for each_friend in userList:
                 lunchFriends.append(each_friend)
             self.addLunchFriend(user, lunchFriends)
-            print "2", lunchFriends
-            # for new_friend in lunchFriends:
-            #     print "2", new_friend.first_name
         
         # userList is greater than 3 or higher
         else:
             # when the number of newFriendsList is less than 2, add all the newFriendsList to 
             # lunchFriends, find the rest from old friends
             if len(newFriendsList) < 2:
-                # lunchFriends = newFriendsList
                 for each_friend in newFriendsList:
                     lunchFriends.append(each_friend)
                 number_of_friends_needed = count_of_friends - len(lunchFriends)
-                # print "needed", number_of_friends_needed
                 oldFriends = random.sample(oldLunchFriends, number_of_friends_needed)
                 lunchFriends += oldFriends
-                print "3", lunchFriends
-                # print "3 lunch", lunchFriends
-                # print "old", oldFriends
                 self.addLunchFriend(user, newFriendsList)
-                # for new_friend in lunchFriends:
-                #     print "3", new_friend.first_name
             
             # if the new friends are between 2 and 4, assign all of them to lunchFriends
             elif len(newFriendsList) >= min_friend_number and len(newFriendsList) <= max_friend_number:
                 for each_friend in newFriendsList:
                     lunchFriends.append(each_friend)
-                print "4", lunchFriends
                 self.addLunchFriend(user, newFriendsList)
-                # for new_friend in lunchFriends:
-                #     print "4", new_friend.first_name
            
-            # if the new friends are over 4
+            # if the new friends are over 4, generate a group with all new friends
+            # between 2 and 4
             else:
                 lunchFriends = random.sample(newFriendsList, count_of_friends)
-                print "5", lunchFriends
                 self.addLunchFriend(user, lunchFriends)
-                # for new_friend in lunchFriends:
-                #     print "5", new_friend.first_name
         return (True, lunchFriends)
 
     # this function takes the user in session and friends list (called newFriendsList in the 
--- apps/first_app/tests.py
+++ apps/first_app/tests.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
-from first_app.models import User
+from .models import User
+from django.db.models import Manager
 
-# Create your tests here.
-class CoffeeTest(TestCase):
+# class UserMethodTestCase(TestCase):
+#     def setUp(self):
+#         pass
+#     def test_getCoffee(self):
+#         users = User.objects.all()
+#         if len(users) > 0:
+#             id = 1
+#         else:
+#             id = 0
+#         user = User.objects.get(id=id)
+#         self.assertNotEqual(User.objects.getCoffee(pid=id), id)
